a29f886a253a9c2bd901a7e1438ab802f4ee1c9c
Siva <sivansbhfe@gmail.com>
2020-05-24 16:11:28 +0530

    Dynamic assignment of table data

--- src/main/java/com/bold/appws/ui/modelrequest/TemplateDetailsRequestModel.java
+++ src/main/java/com/bold/appws/ui/modelrequest/TemplateDetailsRequestModel.java
@@ -2,6 +2,89 @@ package com.bold.appws.ui.modelrequest;
 
 public class TemplateDetailsRequestModel {
 	private String title;
+	private String platform;
+	private String jira;
+	private String servicenow;
+	private String webstar;
+	private String status;
+	private String incimanager;
+	private String incimanageremail;
+	private String starttime;
+	private String endtime;
+
+
+
+	public String getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(String platform) {
+		this.platform = platform;
+	}
+
+	public String getJira() {
+		return jira;
+	}
+
+	public void setJira(String jira) {
+		this.jira = jira;
+	}
+
+	public String getServicenow() {
+		return servicenow;
+	}
+
+	public void setServicenow(String servicenow) {
+		this.servicenow = servicenow;
+	}
+
+	public String getWebstar() {
+		return webstar;
+	}
+
+	public void setWebstar(String webstar) {
+		this.webstar = webstar;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getIncimanager() {
+		return incimanager;
+	}
+
+	public void setIncimanager(String incimanager) {
+		this.incimanager = incimanager;
+	}
+
+	public String getIncimanageremail() {
+		return incimanageremail;
+	}
+
+	public void setIncimanageremail(String incimanageremail) {
+		this.incimanageremail = incimanageremail;
+	}
+
+	public String getStarttime() {
+		return starttime;
+	}
+
+	public void setStarttime(String starttime) {
+		this.starttime = starttime;
+	}
+
+	public String getEndtime() {
+		return endtime;
+	}
+
+	public void setEndtime(String endtime) {
+		this.endtime = endtime;
+	}
 
 	public String getTitle() {
 		return title;
