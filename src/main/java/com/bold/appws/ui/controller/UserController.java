3d3dd6edc36867de8605cfb9d2006e131e597db5
Siva <sivansbhfe@gmail.com>
2020-05-24 14:25:23 +0530

    Created basic html template for email body

--- src/main/java/com/bold/appws/ui/controller/UserController.java
+++ src/main/java/com/bold/appws/ui/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.bold.appws.ui.controller;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,49 +25,57 @@ public class UserController {
 	})
 	public String postData(@RequestBody TemplateDetailsRequestModel templateDetails) {
 		System.out.println("Title:" + templateDetails.getTitle());
+		String notTitle = templateDetails.getTitle();
 		
-		return "	<table border=\"1\">\r\n" + 
-				"							<colgroup>\r\n" + 
-				"		<col style={{width:\"250px\"}}/>\r\n" + 
-				"		<col style={{width:\"600px\"}}/>\r\n" + 
-				"	  </colgroup><tbody>\r\n" + 
-				"			<tr id=\"tabletitle\" style={{fontSize: \"50px\", fontFamily: \"calibri\", color:\"rgb(102, 148, 102)\", \r\n" + 
-				"				textAlign: \"center\", height: \"100px\"}}>\r\n" + 
-				"					<td colSpan=\"2\">{this.state.title}</td>\r\n" + 
-				"					</tr>\r\n" + 
-				"		<tr id=\"tableticket\">\r\n" + 
-				"			<td colSpan=\"2\"><span>Ticket information: </span>\r\n" + 
-				"			<span>{this.state.jiraticket!==\"\" ? <span>JIRA: <a href={this.state.jiraticket}>{this.state.jiraticket}</a> </span>:<span></span>}\r\n" + 
-				"			</span>\r\n" + 
-				"			<span>{this.state.snticket!==\"\" ? <span>ServiceNow: <a href={this.state.snticket}>{this.state.snticket}</a> </span>:<span></span>}\r\n" + 
-				"			</span>\r\n" + 
-				"			<span>{this.state.webstarticket!==\"\" ? <span>Webstar: <a href={this.state.webstarticket}>{this.state.webstarticket}</a></span>:<span></span>}\r\n" + 
-				"			</span></td>\r\n" + 
-				"			</tr>\r\n" + 
-				"			<tr><td style={{textAlign:\"right\",fontFamily: \"calibri\",fontWeight:\"bold\", fontSize:\"20px\"}} \r\n" + 
-				"			id=\"tableplatform\" >Platform</td>\r\n" + 
-				"			<td>{this.state.platform}</td></tr>\r\n" + 
-				"			<tr><td style={{textAlign:\"right\",fontFamily: \"calibri\",fontWeight:\"bold\", fontSize:\"20px\"}} \r\n" + 
-				"			id=\"tablestatus\" >Status</td>\r\n" + 
-				"			<td>{this.state.status}</td></tr>\r\n" + 
-				"			<tr><td style={{textAlign:\"right\",fontFamily: \"calibri\",fontWeight:\"bold\", fontSize:\"20px\"}} \r\n" + 
-				"			id=\"tabletime\" >Start time & End time</td>\r\n" + 
-				"			<td>{this.state.timewindow}</td></tr>\r\n" + 
-				"			{this.state.incidentmanager!==\"\" ?\r\n" + 
-				"			<tr><td style={{textAlign:\"right\",fontFamily: \"calibri\",fontWeight:\"bold\", fontSize:\"20px\"}} \r\n" + 
-				"			id=\"tabletime\" >Incident Manager</td>\r\n" + 
-				"			<td><a href={this.state.incidentmanageremail}> {this.state.incidentmanager}</a></td></tr> : \"\"\r\n" + 
-				"			}\r\n" + 
-				"			<tr><td style={{textAlign:\"right\",fontFamily: \"calibri\",fontWeight:\"bold\", fontSize:\"20px\"}} \r\n" + 
-				"			id=\"tablesummary\" >Summary</td>\r\n" + 
-				"			<td>{this.state.summary}</td></tr>\r\n" + 
-				"			<tr><td colSpan=\"2\" style={{fontFamily: \"calibri\",fontSize:\"20px\"}}>Please contact  GTO APAC Content Support via reply to this \r\n" + 
-				"				<a href=\"mailto:sivabalan.shanmugasiva@lexisnexis.com\">email</a> if you have any queries/concerns.</td></tr>\r\n" + 
-				"			<tr><td colSpan=\"2\" style={{fontFamily: \"calibri\",fontWeight:\"bold\", fontSize:\"20px\"}}>Regards<br/>GTO APAC Content Support<br/>\r\n" + 
-				"			Email: <a href=\"mailto:sivabalan.shanmugasiva@lexisnexis.com\">CSS (LNG - AUS)</a><br/>\r\n" + 
-				"			<span style={{fontSize:\"15px\"}}>Note: Internal & External</span>\r\n" + 
-				"			</td></tr>\r\n" + 
-				"			</tbody>\r\n" + 
-				"							</table>";
+		String htmlbody = "<html>\r\n" + 
+				"<table border=\"1\">\r\n" + 
+				"<colgroup>\r\n" + 
+				"<col style=\"width:250px\"/>\r\n" + 
+				"<col style=\"width:600px\"/>\r\n" + 
+				"</colgroup>\r\n" + 
+				"<tbody>\r\n" + 
+				"<tr id=\"tabletitle\" style=\"font-size:50px;font-family:calibri;color:rgb(102, 148, 102);text-align:center;height:100px\">\r\n" + 
+				"<td colspan=\"2\">"+notTitle+"</td>\r\n" + 
+				"</tr>\r\n" + 
+				"<tr id=\"tableticket\">\r\n" + 
+				"<td colspan=\"2\"><span>Ticket information: </span>\r\n" + 
+				"<span>JIRA: </span><span>jira</span>\r\n" + 
+				"<span>ServiceNow: </span><span>servicenow</span>\r\n" + 
+				"<span>Webstar: </span><span>webstar</span>\r\n" + 
+				"</td>\r\n" + 
+				"</tr>\r\n" + 
+				"<tr><td style=\"text-align:right;font-family:calibri;font-weight:bold;font-size:20px\" id=\"tableplatform\" >Platform</td>\r\n" + 
+				"<td>platform</td></tr>\r\n" + 
+				"<tr><td style=\"text-align:right;font-family:calibri;font-weight:bold;font-size:20px\" id=\"tableplatform\" >Status</td>\r\n" + 
+				"<td>Status</td></tr>\r\n" + 
+				"<tr><td style=\"text-align:right;font-family:calibri;font-weight:bold;font-size:20px\" id=\"tableplatform\" >Start time & End time</td>\r\n" + 
+				"<td>Start time & End time</td></tr>\r\n" + 
+				"<tr><td style=\"text-align:right;font-family:calibri;font-weight:bold;font-size:20px\" id=\"tableplatform\" >Incident Manager</td>\r\n" + 
+				"<td>Incident Manager</td></tr>\r\n" + 
+				"<tr><td colspan=\"2\" style=\"font-family:calibri;font-size:12px\">Please contact  GTO APAC Content Support via reply to this \r\n" + 
+				"<a href=\"mailto:sivabalan.shanmugasiva@lexisnexis.com\">email</a> if you have any queries/concerns.</td></tr>\r\n" + 
+				"<tr><td colspan=\"2\" style=\"font-family:calibri;font-size:14px\">Regards<br/>GTO APAC Content Support<br/>\r\n" + 
+				"Email: <a href=\"mailto:sivabalan.shanmugasiva@lexisnexis.com\">CSS (LNG - AUS)</a><br/>\r\n" + 
+				"<span style=\"fon-size:15px\">Note: Internal & External</span>\r\n" + 
+				"</td></tr>\r\n" + 
+				"</tbody>\r\n" + 
+				"</table>\r\n" +
+				"</html>";
+		File file = new File("C:\\Users\\Sivabalan\\Desktop\\test.html");
+		BufferedWriter bw;
+		try {
+			bw = new BufferedWriter(new FileWriter(file));
+			try {
+				bw.write(htmlbody);
+				bw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return "text";
 	}
 }
