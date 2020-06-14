0a99deaad6497baff9d163a53a51013228e81a16
Siva <sivansbhfe@gmail.com>
2020-06-14 18:28:10 +0530

    Resolved issues,tomcat version and serverlet initializer

--- src/main/java/com/bold/appws/MobileAppNotificationApplication.java
+++ src/main/java/com/bold/appws/MobileAppNotificationApplication.java
@@ -2,9 +2,10 @@ package com.bold.appws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MobileAppNotificationApplication  {
+public class MobileAppNotificationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppNotificationApplication.class, args);
